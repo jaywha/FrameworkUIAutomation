8e932f5cde2fb94d7e7f8e80369107b74969d7c9
Jay Whaley <jwhaley@MARKETRON.NET>
2022-06-23 14:39:55 -0500

    Add project files.

---
+++ FrameworkUITesting/MainTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace FrameworkUITesting
+{
+    [TestClass]
+    public class MainTests
+    {
+        private static WindowsDriver<WindowsElement> Driver;
+        private const string TESTING_OUT_DIR = @"C:\MyTestingFolder\";
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            var appiumOptions = new OpenQA.Selenium.Appium.AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app",
+                @"C:\Users\jwhaley\source\repos\FrameworkUIAutomation\FrameworkUIAutomation\bin\Debug\FrameworkUIAutomation.exe");
+            appiumOptions.AddAdditionalCapability("appWorkingDir", TESTING_OUT_DIR);
+
+            Driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+            File.WriteAllText($"{TESTING_OUT_DIR}{DateTime.Now:MM-dd-yyyy}_UITestResults.log", Driver.PageSource);
+        }
+
+        [TestMethod]
+        public void ButtonClickTest()
+        {
+            // Press Button and check label
+            var btnTestMe = Driver.FindElementByName("button1");
+            var lblResultText = Driver.FindElementByName("label1");
+            btnTestMe.Click();
+            Thread.Sleep(2000);
+            Assert.IsTrue(lblResultText.Text.Equals("Clicked!"));
+            Thread.Sleep(2000);
+            //
+        }
+
+        [TestMethod]
+        public void CheckboxClickTest()
+        {
+            // Press CheckBox
+            var chkbxCheckMe = Driver.FindElementByName("checkBox1");
+            chkbxCheckMe.Click();
+            // Assert.True(chkbxCheckMe.)
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Driver.Quit();
+            Driver = null;
+        }
+    }
+}
---
+++ FrameworkUITesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrameworkUITesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameworkUITesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd3949bd-e730-4409-9584-e99f1e95a925")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
